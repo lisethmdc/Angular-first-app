e4bb507ef328719687d7db4a337fb79fd9405fe1
lisethmdc <lismc1992@gmail.com>
2022-04-06 13:15:48 +0200

    finish detail movie

--- src/app/pages/movie-list/movie-detail/movie-detail.component.ts
+++ src/app/pages/movie-list/movie-detail/movie-detail.component.ts
@@ -1,31 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-// import { MovieDetail } from 'src/app/models/detail.model';
-import { MovieListService } from '../movie-list.service';
+import { ActivatedRoute } from '@angular/router';
+import { DetailInterface } from 'src/app/models/detail.model';
+import { MovieDetailService } from './movie-detail.service';
 
 @Component({
   selector: 'app-movie-detail',
   templateUrl: './movie-detail.component.html',
   styleUrls: ['./movie-detail.component.scss']
 })
-export class MovieDetailComponent implements OnInit {
-  // public movieDetail: MovieDetail[];
 
-  constructor(private movieListService: MovieListService) { }
+export class MovieDetailComponent implements OnInit {
+    public movieId: string = '';
+    public movie?: DetailInterface;
+    
+  constructor(private route: ActivatedRoute,
+              private movieDetailService: MovieDetailService) { }
 
   ngOnInit(): void {
-    // this.getMovieDetail()
+    this.recoverMovieId();
   }
+ 
+    private getDetail(id: string): void {
+      this.movieDetailService.getMovieDetail(id).subscribe((res: any) => {
+        this.movie = res;
+      })
+    }
 
-  /* getMovieDetail(): void{
-    this.movieListService.getMovieDetail().subscribe((res: MovieDetail[])=> {
-      this.movieDetail = res;
-      //this.movieList = res;
-    })
-  } */
-
-/*
-  public viewDetail(item: MovieDetail): void {    
-    this.getMovieDetail(item.url)
-  }  */
+    recoverMovieId(): void{
+      this.route.params.subscribe((params) => {
+        this.movieId = params['id']
+        this.getDetail(this.movieId)  
+      })}
 
 }
